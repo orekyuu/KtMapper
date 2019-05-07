1d8a9fea819fadf664121a4860e554a17599cf9c
orekyuu <orekyuu@gmail.com>
2019-05-07 22:51:32 +0900

    複合主キーのテスト

--- src/test/kotlin/net/orekyuu/ktmapper/MappingBuilderTest.kt
+++ src/test/kotlin/net/orekyuu/ktmapper/MappingBuilderTest.kt
@@ -63,6 +63,46 @@ internal class MappingBuilderTest {
     }
 
     @Test
+    fun mappingFlatModelWithCompositePK() {
+        val testData = listOf(
+            mapOf("receipt_id" to 1L, "created_at" to LocalDate.of(2019, 5, 5).atStartOfDay(),
+                "line_item_id" to 1L, "quantity" to 1L, "item_id" to 1L, "name" to "item1"),
+            mapOf("receipt_id" to 1L, "created_at" to LocalDate.of(2019, 5, 6).atStartOfDay(),
+                "line_item_id" to 2L, "quantity" to 3L, "item_id" to 2L, "name" to "item2"),
+            mapOf("receipt_id" to 1L, "created_at" to LocalDate.of(2019, 5, 6).atStartOfDay(),
+                "line_item_id" to 3L, "quantity" to 3L, "item_id" to 1L, "name" to "item1")
+        )
+
+        val rowMapper = mapping<Map<String, Any>, Receipt> {
+            primaryKey { Pair(it["receipt_id"] as Long, it["created_at"] as LocalDateTime) }
+
+            val lineItemRef = hasMany<LineItem> {
+                primaryKey { it["line_item_id"] as Long }
+
+                val itemRef = hasOne<Item> {
+                    domain {
+                        Item(it["item_id"] as Long, it["name"] as String)
+                    }
+                }
+
+                domain {
+                    LineItem(findOne(itemRef, it)!!, it["quantity"] as Long)
+                }
+            }
+
+            domain {
+                Receipt(it["receipt_id"] as Long, it["created_at"] as LocalDateTime, findList(lineItemRef, it))
+            }
+        }
+
+        val toList = rowMapper.mappingList(testData)
+        Assertions.assertThat(toList).isEqualTo(listOf(
+            Receipt(1L, LocalDate.of(2019, 5, 5).atStartOfDay(), listOf(LineItem(Item(1L, "item1"), 1))),
+            Receipt(1L, LocalDate.of(2019, 5, 6).atStartOfDay(), listOf(LineItem(Item(2L, "item2"), 3), LineItem(Item(1L, "item1"), 3)))
+        ))
+    }
+
+    @Test
     fun mappingRelation() {
         val rowMapper = mapping<Map<String, Any>, Receipt> {
             primaryKey { it["receipt_id"] as Long }
